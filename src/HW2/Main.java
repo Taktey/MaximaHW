76f7c676a1ca0d99dc50bced1e4841f33485a6e0
taktey <79062502914@yandex,ru>
2024-02-01 21:08:10 +0300

    HW 01.02.24

--- src/HW2/Main.java
+++ src/HW2/Main.java
@@ -1,25 +1,64 @@
 package HW2;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        System.out.println(task3());
+        task4();
     }
+
     public static void task1() {
         System.out.println("Задача 1");
-        // Пишем код для задачи 1
+        int[] array = new int[5];
+        for (int i = 0; i < 5; i++) {
+            array[i] = (int) (Math.random() * 10);
+        }
+        System.out.println(Arrays.toString(array));
     }
 
     public static int task2() {
         System.out.println("Задача 2");
-        // Пишем код для задачи 2
+        int[] array = new int[10];
+        for (int i = 1; i < 10; i++) {
+            array[i - 1] = i;
+            if (i % 2 != 0) System.out.print(i + ", ");
+        }
+        System.out.println();
+        return 0;
+    }
+
+    public static double task3() {
+        System.out.println("Задача 3");
+        double[] array = new double[10];
+        double res = 0;
+        for (int i = 0; i < 10; i++) {
+            array[i] = Math.random() * 20;
+            res += array[i];
+        }
+        return res;
+    }
+
+    public static void task4() {
+        System.out.println("Задача 4");
+        String[] array = {"e", "d", "c", "b", "a"};
+        System.out.println(concat(array));
+    }
+
+    public static String concat(String[] someArray) {
+        String res = "";
+        for (int i = 0; i < 5; i++) {
+            res += someArray[i];
+        }
+        return res;
     }
 }
 
 
 //ЗАДАНИЕ:
-//0) провести небольшой рефакторинг кода прошлой домашки - переместить код в метод doFirstHomework(), чтобы в методе main остался только вызов этого метода (либо же создать новый проект - по желанию)
-//
+
 //далее таски (task1...n) делать в новых методах с названиями "task1...n" и также вызывать их из метода main
 //
 //task1) объявить пустой массив типа int[] с заданным размером, в цикле заполнить его случайными значениями (с помощью Random), далее вывести весь массив в консоль с помощью Arrays.toString()
