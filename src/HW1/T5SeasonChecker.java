06ff2937286b19ba9cd78654d997a67200c618a5
taktey <79062502914@yandex,ru>
2024-02-01 20:07:15 +0300

    HW 29.01.24, refactor

--- src/HW1/T5SeasonChecker.java
+++ src/HW1/T5SeasonChecker.java
@@ -4,40 +4,24 @@ public class T5SeasonChecker {
         if (monthNumber <= 12 && monthNumber >= 0) {
             switch (monthNumber) {
                 case (1):
-                    System.out.println("Зима");
-                    break;
                 case (2):
+                case (12):
                     System.out.println("Зима");
                     break;
                 case (3):
-                    System.out.println("Весна");
-                    break;
                 case (4):
-                    System.out.println("Весна");
-                    break;
                 case (5):
                     System.out.println("Весна");
                     break;
                 case (6):
-                    System.out.println("Лето");
-                    break;
                 case (7):
-                    System.out.println("Лето");
-                    break;
                 case (8):
                     System.out.println("Лето");
                     break;
                 case (9):
-                    System.out.println("Осень");
-                    break;
                 case (10):
-                    System.out.println("Осень");
-                    break;
                 case (11):
                     System.out.println("Осень");
-                    break;
-                case (12):
-                    System.out.println("Зима");
                     break;
             }
         }
